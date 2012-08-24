a16c8437579c9492e73cd3163553bc857d750bda
Matt McClure <matt@htbx.net>
2012-08-24 14:57:30 -0400

    initial commit

---
+++ monitor.rb
@@ -0,0 +1,20 @@
+# server.rb
+require 'sinatra'
+require 'haml'
+require 'yaml'
+
+# Load up the config file
+Settings = YAML.load_file("config.yml") unless defined? CONFIG
+
+# SASS it up.
+get '/styles.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  scss :styles
+end
+
+get '/?:channel?' do
+	@channel = params[:channel]
+	@publish = Settings['publish']
+	@subscribe = Settings['subscribe']
+  haml :index
+end
